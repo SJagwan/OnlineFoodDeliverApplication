97d127d82a36194b80bed8b7bccaa0ba60e33b9d
tyagimayank027 <tyagimayank027@gmail.com>
2021-03-23 16:56:46 +0530

    getter setter generated for restaurant

--- src/main/java/com/cg/fds/entities/Restaurant.java
+++ src/main/java/com/cg/fds/entities/Restaurant.java
@@ -3,11 +3,63 @@ package com.cg.fds.entities;
 import java.util.List;
 
 public class Restaurant {
-private String restaurantId;
-private String restaurantName;
-private Address address;
-private List<Item> itemList;
-private String managerName;
-private String contactNumber;
+	private String restaurantId;
+	private String restaurantName;
+	private Address address;
+	private List<Item> itemList;
+	private String managerName;
+	private String contactNumber;
+
+	public Restaurant() {
+
+	}
+
+	public String getRestaurantId() {
+		return restaurantId;
+	}
+
+	public void setRestaurantId(String restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+
+	public String getRestaurantName() {
+		return restaurantName;
+	}
+
+	public void setRestaurantName(String restaurantName) {
+		this.restaurantName = restaurantName;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public List<Item> getItemList() {
+		return itemList;
+	}
+
+	public void setItemList(List<Item> itemList) {
+		this.itemList = itemList;
+	}
+
+	public String getManagerName() {
+		return managerName;
+	}
+
+	public void setManagerName(String managerName) {
+		this.managerName = managerName;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
 
 }
