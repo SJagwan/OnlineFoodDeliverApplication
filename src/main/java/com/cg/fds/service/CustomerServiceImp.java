c002746d38c33b05e58e57e07d61fc8c79a1d174
adityaakundi <adityavi67@gmail.com>
2021-03-24 12:08:59 +0530

    created test class

--- src/main/java/com/cg/fds/service/CustomerServiceImp.java
+++ src/main/java/com/cg/fds/service/CustomerServiceImp.java
@@ -1,5 +1,40 @@
 package com.cg.fds.service;
 
-public class CustomerServiceImp {
+import java.util.List;
 
+import com.cg.fds.entities.Customer;
+
+public class CustomerServiceImp implements ICustomerService{
+
+	@Override
+	public Customer addCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer updateCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer removeCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer viewCustomer(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Customer> viewAllCustomer(String restaurantname) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
