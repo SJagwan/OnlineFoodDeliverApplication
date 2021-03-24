f0138ac51662f720b56d78fd59612d3724cb0c8b
siddharth-s1001 <siddharthtps@gmail.com>
2021-03-24 23:03:12 +0530

    testcase updated till update and view

--- src/main/java/com/cg/fds/service/BillServiceImp.java
+++ src/main/java/com/cg/fds/service/BillServiceImp.java
@@ -1,5 +1,68 @@
 package com.cg.fds.service;
 
-public class BillServiceImp {
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cg.fds.entities.Bill;
+import com.cg.fds.repository.IBillRepository;
+
+
+@Service 
+public class BillServiceImp implements IBillService{
+	
+	@Autowired
+	private IBillRepository billRepository;
+
+	@Override
+	public Bill addBill(Bill bill) {
+		return billRepository.save(bill);
+	}
+
+	@Override
+	public Bill updateBill(Bill bill) {
+		
+		 return billRepository.save(bill);
+	}
+
+	@Override
+	public Bill removeBill(Bill bill) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bill viewBill(Bill bill) {
+		Optional<Bill> viewBill = billRepository.findById(bill);
+		Bill newBill = null;
+		if(viewBill.isPresent()) {
+			newBill = viewBill.get();
+		}
+		return bill;
+	}
+
+	@Override
+	public List<Bill> viewBills(LocalDate startDate, LocalDate endDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Bill> viewBills(String custId) {
+		
+		return null;
+	}
+	
+	@Override
+	public double totalCost(Bill bill) {
+		return billRepository.totalCost(bill);
+	}
+	 
+
+	
 
 }
