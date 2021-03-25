2de1c0df88a86f7d8a55748c409ea7cb41e1b43d
VREXA12 <pvpranavverma12@gmail.com>
2021-03-25 18:42:18 +0530

    Item Service updated

---
+++ src/main/java/com/cg/fds/exception/RemoveItemException.java
@@ -0,0 +1,8 @@
+package com.cg.fds.exception;
+
+public class RemoveItemException extends RuntimeException {
+public RemoveItemException(String msg)
+{
+super(msg);	
+}
+}
--- src/main/java/com/cg/fds/repository/IItemRepository.java
+++ src/main/java/com/cg/fds/repository/IItemRepository.java
@@ -1,19 +1,30 @@
 package com.cg.fds.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.cg.fds.entities.Category;
 import com.cg.fds.entities.Item;
-import com.cg.fds.entities.Restaurant;
 
 public interface IItemRepository {
+	 Item save(Item item);
 
-	public Item addItem(Item item);
+	//public List<Item> findByLocation(String Location);
+
+	//public List<Item> findRestaurantByItemName(String name);
+
+	 Item remove(String itemid);
+	 Optional<Item> findById(String itemid);
+	
+	boolean existsById(String itemId);
+
+	
+	/*	public Item addItem(Item item);
 	public Item viewItem(String id);
 	public Item updateItem(Item item);
 	public Item removeItem(String id);
 	public List<Item> viewAllItems(Restaurant res);
 	public List<Item> viewAllItems(Category cat);
 	public List<Item> viewAllItemsByName(String name);
-	
+	*/
 }
