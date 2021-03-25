b3a468c3b9377d88aefe84fb2387c421d905e0af
adityaakundi <adityavi67@gmail.com>
2021-03-25 18:56:43 +0530

    added ICartService class

--- src/main/java/com/cg/fds/repository/ICartRepository.java
+++ src/main/java/com/cg/fds/repository/ICartRepository.java
@@ -5,11 +5,13 @@ import com.cg.fds.entities.FoodCart;
 import com.cg.fds.entities.Item;
 
 public interface ICartRepository{
+	
+	FoodCart save(FoodCart cart);
 
-	public FoodCart addItemToCart(FoodCart cart,Item item);
-	public FoodCart increaseQuantity(FoodCart cart,Item item,int quantity);
-	public FoodCart reduceQuantity(FoodCart cart,Item item,int quantity);
-	public FoodCart removeItem(FoodCart cart,Item item);
-	public FoodCart clearCart(FoodCart cart);
+//	public FoodCart addItemToCart(FoodCart cart,Item item);
+//	public FoodCart increaseQuantity(FoodCart cart,Item item,int quantity);
+//	public FoodCart reduceQuantity(FoodCart cart,Item item,int quantity);
+//	public FoodCart removeItem(FoodCart cart,Item item);
+//	public FoodCart clearCart(FoodCart cart);
 	
 }
--- src/main/java/com/cg/fds/service/CartServiceImp.java
+++ src/main/java/com/cg/fds/service/CartServiceImp.java
@@ -1,14 +1,28 @@
 package com.cg.fds.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.cg.fds.entities.FoodCart;
 import com.cg.fds.entities.Item;
+import com.cg.fds.repository.ICartRepository;
 
 public class CartServiceImp implements ICartService {
-
+    @Autowired
+    private ICartRepository cartRepository;
 	@Override
 	public FoodCart addItemToCart(FoodCart cart, Item item) {
-		// TODO Auto-generated method stub
-		return null;
+    List <Item> items = cart.getItemList();
+    if(items==null) {
+    	items = new ArrayList<>();
+    	cart.setItemList(items);
+    }
+    items.add(item);
+		cart.setItemList(items);
+		
+		return cartRepository.save(cart);
 	}
 
 	@Override
