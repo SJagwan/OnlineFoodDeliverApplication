3caa73d62408d392edee375da261e0b5f8ef26ab
tyagimayank027 <tyagimayank027@gmail.com>
2021-03-26 16:46:54 +0530

    Jpa repository

--- src/main/java/com/cg/fds/repository/IRestaurantRepository.java
+++ src/main/java/com/cg/fds/repository/IRestaurantRepository.java
@@ -3,20 +3,23 @@ package com.cg.fds.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.cg.fds.entities.Restaurant;
 
-public interface IRestaurantRepository {
-	public Restaurant save(Restaurant restaurant);
-
-	public List<Restaurant> findByLocation(String Location);
-
-	public List<Restaurant> findRestaurantByItemName(String name);
-
-	public Restaurant remove(Restaurant restaurant);
-
-	public Optional<Restaurant> findById(String id);
+public interface IRestaurantRepository extends JpaRepository<Restaurant,String>{
 	
-	boolean existsById(String restaurantId);
+//	public Restaurant save(Restaurant restaurant);
+//
+public List<Restaurant> findByLocation(String Location);
+//
+public List<Restaurant> findRestaurantByItemName(String name);
+//
+//	public Restaurant remove(Restaurant restaurant);
+//
+//	public Optional<Restaurant> findById(String id);
+//	
+//	boolean existsById(String restaurantId);
 	/*
 	 * public Restaurant addRestaurant(Restaurant res); public Restaurant
 	 * removeRestaurant(Restaurant res); public Restaurant
